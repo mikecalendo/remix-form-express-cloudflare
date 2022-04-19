ebd64e2b80a5e98cf85d3d6c1c74dfc484ccef2c
mikecalendo <contact@michaelcalendo.com>
2022-04-18 22:25:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-d7qvhs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-d7qvhs)
---
+++ app/components/SiteLayout.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  // Put Header or Footer Here
+  return <>{children}</>;
+}
